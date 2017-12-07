4da37fe32663f795035f0bba720f62c6814a9456
acf2 <acrfnv@gmail.com>
2017-12-07 10:02:29 +0700

    Add number theory support
    Miller-Rabin check for primality, in particular.

--- forward-secure-signing.lisp
+++ forward-secure-signing.lisp
@@ -11,8 +11,8 @@
   (with-output-to-string (out)
     (s-base64:encode-base64-bytes arr out)))
 
-; TODO make it 4096 when you implement miller-rabin
-(defparameter *boundary* 64)
+; TODO make it 4096
+(defparameter *boundary* 256)
 
 (defun generate-blum-number ()
   (loop for num = (number-theory:generate-prime (/ *boundary* 2)) when (eql (mod num 4) 3) return num))
@@ -22,4 +22,4 @@
          (q (generate-blum-number))
          (N (* p q)))
     (format t "~A * ~A = ~A~%" p q N)
-    (format t "~%~%~A ~A~%" (mod p 4) (mod q 4))))
+    (format t "ll~A ~A~%" (mod p 4) (mod q 4))))
--- number-theory.lisp
+++ number-theory.lisp
@@ -1,26 +1,68 @@
 (in-package :number-theory)
 
-;; Quickly finds b^e (mod m).
-(defun exptmod (b e m)
-  (if (zerop e) 1
-    (mod (if (evenp e) 
-             ((lambda (x) (* x x)) (exptmod b (/ e 2) m))
-             (* b (exptmod b (1- e) m)))
-         m)))
-
 (defparameter *package-random-state* (make-random-state t))
 
 (defun generate-fixed-size-number (bits)
-  (random (expt 2 bits) *package-random-state*))
+  (logior (ash 1 (1- bits)) (random (ash 1 bits) *package-random-state*)))
 
-(defun prime? (x)
-  (cond
-    ((= x 1) nil)
-    ((= x 2) t)
-    ((= (mod x 2) 0) nil)
-    (t (let ((bound (ceiling (sqrt x))))
-         (loop for a from 3 to bound by 2
-               never (= (mod x a) 0))))))
+;; makes a binary expansion of an integer in big-endian format
+;; returns binary-array
+(defun binary-expansion (value)
+  (loop with arry = (make-array (integer-length value) :element-type 'bit)
+        for i from 0 to (1- (integer-length value))
+        do (setf (bit arry i) (ldb (byte 1 i) value))
+        finally (return arry)))
+
+;; log(n) power function
+;; can offer reuse of exponent binary expansion
+(defun exptmod (base exponent modulo &key (binary-exponent nil binary-exponent-p))
+  (let ((exponent (if (and binary-exponent-p binary-exponent)
+                    exponent
+                    (binary-expansion exponent))))
+    (loop with result = 1
+          for i from (1- (length exponent)) downto 0
+          do (setf result (* result result))
+          if (eq (bit exponent i) 1) do (setf result (* result base))
+            do (setf result (mod result modulo))
+          finally (return result))))
+
+;; binary search for extracting all powers of 2 from number
+;; let z be the number of tail zeros in given number
+;; first loop: find such t that (< (expt 2 t) z (expt 2 (1+ t))) is T
+;; second loop: for every s from t-1 downto 0, if (<= (+ result (ash 1 s)) z) is T, then add (ash 1 s) to result
+(defun tail-zeros (num)
+  "With binary search find number of all tailing zeros for binary representation of given integer"
+  (let ((zero-tail-upper-bound-power-of-2 0))
+    (loop while (eq (ldb (byte (ash 1 (1+ zero-tail-upper-bound-power-of-2)) 0) num) 0)
+          do (setf zero-tail-upper-bound-power-of-2
+                   (1+ zero-tail-upper-bound-power-of-2)))
+    (loop with result = (ash 1 zero-tail-upper-bound-power-of-2)
+          for test-power-of-2 from (1- zero-tail-upper-bound-power-of-2) downto 0
+          if (eq (ldb (byte (+ result (ash 1 test-power-of-2)) 0) num) 0)
+            do (setf result (+ result (ash 1 test-power-of-2)))
+          finally (return result))))
+
+(defun prime? (test-number &optional (reliability 20))
+  "Miller-Rabin primality test
+NIL return means \"composite\" and is true for all occurances
+T return means \"prime\" and can be error with probability (expt 1/4 reliability)"
+  (if (eq (mod test-number 2) 0)
+    (return-from prime? nil)
+    (let* ((power-of-2 (tail-zeros (1- test-number)))
+           (even-part (ash (1- test-number) (- power-of-2))))
+      (loop for i from 1 to reliability
+            with test-base and chain-part
+            do (setf test-base (+ (random (- test-number 3) *package-random-state*) 2))
+            do (setf chain-part (exptmod test-base even-part test-number))
+            if (not (or (= chain-part 1) (= chain-part (1- test-number))))
+            do (loop with j = 1
+                     while (and (not (= chain-part (1- test-number))) (< j power-of-2))
+                     do (setf chain-part (exptmod chain-part 2 test-number))
+                     if (= chain-part 1) do  (return-from prime? nil)
+                     do (setf j (1+ j))
+                     finally (when (not (= chain-part (1- test-number)))
+                               (return-from prime? nil)))
+            finally (return-from prime? t)))))
 
 (defun generate-prime (bits)
   (loop for x = (logior (ash 1 (1- bits)) 1 (random (ash 1 bits)))
